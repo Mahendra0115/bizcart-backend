3e1c37d34caf9987aaad34d98042ecdc9bb19331
Mahendra0115 <mahendrasing0115@gmail.com>
2026-07-08 11:46:33 +0530

    chore: initialize BizCart Spring Boot backend

---
+++ src/main/java/com/mahendra/bizcart_backend/BizcartBackendApplication.java
@@ -0,0 +1,13 @@
+package com.mahendra.bizcart_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BizcartBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BizcartBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mahendra/bizcart_backend/BizcartBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mahendra.bizcart_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BizcartBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
